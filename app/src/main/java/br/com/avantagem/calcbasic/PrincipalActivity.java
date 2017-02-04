9e202b4e8efb6c75a81e08ce524ca2726ec9cc3a
adriano barbosa <b.adrianobarbosa@gmail.com>
2017-02-04 13:27:36 -0300

    Calculadora básica

--- app/src/main/java/br/com/avantagem/calcbasic/PrincipalActivity.java
+++ app/src/main/java/br/com/avantagem/calcbasic/PrincipalActivity.java
@@ -1,8 +1,11 @@
 package br.com.avantagem.calcbasic;
 
+import android.renderscript.Double2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class PrincipalActivity extends AppCompatActivity {
 
@@ -13,9 +16,30 @@ public class PrincipalActivity extends AppCompatActivity {
     }
 
     public void calcular(View view) {
-        double vl1 = findViewById(R.id.edValor1)
+        EditText ed1 = (EditText) findViewById(R.id.edValor1);
+        EditText ed2 = (EditText) findViewById(R.id.edValor2);
+        TextView tvRes = (TextView) findViewById(R.id.tvResultado);
+        double vl1 = Double.parseDouble(ed1.getText().toString());
+        double vl2 = Double.parseDouble(ed2.getText().toString());
+        double res = 0.0;
         if (view.getId() == R.id.btMais) {
-            
+            res = vl1 + vl2;
+            tvRes.setText(""+res);
         }
+        else if (view.getId() == R.id.btDiv) {
+            if (vl2 > 0) {
+                res = vl1 / vl2;
+                tvRes.setText("" + res);
+            } else {
+                tvRes.setText("Divisão por 0");
+            }
+        } else if (view.getId() == R.id.btMult) {
+            res = vl1 * vl2;
+            tvRes.setText(""+res);
+        } else if (view.getId() == R.id.btMenos) {
+            res = vl1 - vl2;
+            tvRes.setText(""+res);
+        }
+
     }
 }
