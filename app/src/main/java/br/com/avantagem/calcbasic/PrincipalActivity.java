da4702c20231ca8333d6de0e01d5138fa6fb42e8
adriano barbosa <b.adrianobarbosa@gmail.com>
2017-02-04 13:06:44 -0300

    Commit inicial

---
+++ app/src/main/java/br/com/avantagem/calcbasic/PrincipalActivity.java
@@ -0,0 +1,21 @@
+package br.com.avantagem.calcbasic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class PrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+
+    public void calcular(View view) {
+        double vl1 = findViewById(R.id.edValor1)
+        if (view.getId() == R.id.btMais) {
+            
+        }
+    }
+}
